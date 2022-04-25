bb8f4bacd58f17e341290fc5b262f420ae47be46
Anirudh Agnihotri <72650662+AnirudhAgnihotri2902@users.noreply.github.com>
2022-04-25 17:23:22 +0530

    Time: 25 ms (8.60%), Space: 11 MB (11.82%) - LeetHub

---
+++ 841-keys-and-rooms/841-keys-and-rooms.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    unordered_set<int>set;
+    vector<int>vis;
+    void solve(vector<vector<int>>&rooms, int i){
+        vis[i] = 1;
+        set.insert(i);
+        for(auto it: rooms[i]){
+            if(vis[it] == 0){
+                solve(rooms,it);
+            }
+        }
+    }
+    
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        set.insert(0);
+        for(int i = 0; i<rooms.size();i++){
+            vis.push_back(0);
+        }
+        for(int i = 0; i<rooms.size();i++){
+            
+            if(set.find(i) == set.end()){
+                return false;
+            }
+            if(vis[i] == 0){
+                solve(rooms,i);
+            }
+        }
+        return true;
+    }
+};
