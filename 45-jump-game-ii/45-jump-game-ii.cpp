2362557aa797fdd34639bf6ad489c7cc11902cad
Anirudh Agnihotri <72650662+AnirudhAgnihotri2902@users.noreply.github.com>
2022-07-07 17:46:45 +0530

    Time: 478 ms (29.17%), Space: 18.5 MB (6.10%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
+    int n;
+    long long int solve(int ind, vector<int>&nums, vector<long long int>&dp){
+        if(ind>=n){
+            return 1e8;
+        }
+        if(ind == n-1){
+            return 0;
+        }
+        if(dp[ind]!=-1){
+            return dp[ind];
+        }
+        long long int ans = INT_MAX;
+        for(int i = 1; i<=nums[ind]; i++){
+            ans = min(ans, solve(ind+i, nums, dp));
+        }
+        return dp[ind] = 1+ ans;
+    }
     int jump(vector<int>& nums) {
-        int len = nums.size(), steps[len];
-        for (int i = 1; i < len; i++) {
-            steps[i] = INT_MAX;
-        }
-        steps[0] = 0;
-        for (int i = 0; i < len; i++) {
-            for (int lmt = i, j = min(len - 1, i + nums[i]); j > lmt; j--) {
-                if (steps[j] > steps[i] + 1) steps[j] = steps[i] + 1;
-                else break;
-            }
-        }
-        return steps[len - 1];
+        n = nums.size();
+        vector<long long int>dp(n+1, -1);
+        return (int)solve(0, nums, dp);
     }
 };
