1c92d05683e8a2d43c6a2ef1536dd84785373d2d
Anirudh Agnihotri <72650662+AnirudhAgnihotri2902@users.noreply.github.com>
2022-07-09 02:27:27 +0530

    Time: 3 ms (88.64%), Space: 17.5 MB (9.51%) - LeetHub

--- 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.cpp
+++ 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.cpp
@@ -1,29 +1,20 @@
 class Solution {
 public:
-    int solve(int day, vector<int>& days, vector<int>& costs, vector<int>&map, vector<int>&dp){
-        if(day>last){
+    int f(int i, int d, vector<int>& days, vector<int>& costs, vector<vector<int>>& dp){
+        if(i >= days.size())
             return 0;
-        }
-        if(dp[day]!=-1){
-            return dp[day];
-        }
-        if(map[day] == 0){
-            return dp[day] =  solve(day+1, days,costs, map,dp);
-        }
-        int first = costs[0]+ solve(day+1, days,costs, map,dp);
-        int second = costs[1] + solve(day+7, days, costs, map, dp);
-        int third = costs[2] + solve(day+30, days, costs, map, dp);
-        return dp[day] = min({first, second, third});
-        
+        if(d >days[days.size()-1])
+            return 0;
+        if(dp[i][d] != -1) return dp[i][d];
+        if(days[i] <= d) return dp[i][d] = f(i+1, d, days, costs, dp);
+        int one = costs[0] + f(i+1, days[i]+0, days, costs, dp);
+        int seven = costs[1] + f(i+1, days[i]+6, days, costs, dp);
+        int thirty = costs[2] + f(i+1, days[i]+29, days, costs, dp);
+        return dp[i][d] = min({one, seven, thirty});
     }
-    int last;
     int mincostTickets(vector<int>& days, vector<int>& costs) {
-        last = days[days.size()-1];
-        vector<int>dp(last+1,-1);
-        vector<int>map(last+1,0);
-        for(int i = 0; i<days.size(); i++){
-            map[days[i]]++;
-        }
-        return solve(1,days, costs, map, dp);
+        int n = days.size();
+        vector<vector<int>> dp(n+1, vector<int>(days[n-1]+1,-1));
+        return f(0, 0, days, costs, dp);
     }
 };
