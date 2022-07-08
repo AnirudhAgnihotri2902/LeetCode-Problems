9ed7d0c4ece600c0bfef7d7f9d37e06d2480d6de
Anirudh Agnihotri <72650662+AnirudhAgnihotri2902@users.noreply.github.com>
2022-07-09 04:33:29 +0530

    Time: 178 ms (31.58%), Space: 19.5 MB (6.32%) - LeetHub

--- 1473-paint-house-iii/1473-paint-house-iii.cpp
+++ 1473-paint-house-iii/1473-paint-house-iii.cpp
@@ -1,28 +1,35 @@
 class Solution {
 public:
-    int dp[100][100][21];
-    const int mx = 1e7+1;
-    int f(vector<int>& houses, vector<vector<int>>& cost, int target, int idx, int count, int prev){
-        if(idx == houses.size()) return count==target ? 0 : mx;
-        if(dp[idx][count][prev]!=-1) return dp[idx][count][prev];
-        int minCost = mx;
-        if(houses[idx]){
-            if(houses[idx]!=prev) minCost = f(houses,cost,target,idx+1,count+1,houses[idx]);
-            else minCost = f(houses,cost,target,idx+1,count,houses[idx]);
-        }else{
-            for(int j=0;j<cost[0].size();j++){
-                int tmp;
-                if((j+1)!=prev) tmp = cost[idx][j] + f(houses,cost,target,idx+1,count+1,j+1);
-                else tmp = cost[idx][j] + f(houses,cost,target,idx+1,count,j+1);
-                minCost = min(minCost,tmp);
-            }
+    long long f(int i, int k, int p, vector<int>& houses, vector<vector<int>>& cost, vector<vector<vector<int>>>& dp){
+        if(i == houses.size()){
+            if (k == 0)
+                return 0;
+            return INT_MAX;
         }
-        return dp[idx][count][prev] = minCost;
+        if(k < 0)
+            return INT_MAX;
+        if(dp[i][k][p] != -1) return dp[i][k][p];
+        if(houses[i] == 0){
+            long long same = INT_MAX, notSame = INT_MAX;
+            if(p != 0)
+                same = cost[i][p-1] + f(i+1, k, p, houses, cost, dp);
+            for(int itr = 0; itr < cost[0].size(); itr++){
+                if(itr != p-1)
+                    notSame = min(notSame, cost[i][itr] + f(i+1, k-1, itr+1, houses, cost, dp));
+            }
+            return dp[i][k][p] = min(same, notSame);
+        }
+        else if(houses[i] == p){
+            return dp[i][k][p] = f(i+1, k, p, houses, cost, dp);
+        }
+        else
+            return dp[i][k][p] = f(i+1, k-1, houses[i], houses, cost, dp);
+        
     }
     int minCost(vector<int>& houses, vector<vector<int>>& cost, int m, int n, int target) {
-        memset(dp,-1,sizeof(dp));
-        int ans = f(houses,cost,target,0,0,0);
-        if(ans==mx) return -1;
-        else return ans;
+        vector<vector<vector<int>>> dp(m, vector<vector<int>>(target+1, vector<int>(n+1,-1)));
+        long long ans =  f(0, target, 0, houses, cost, dp);
+        if(ans >= INT_MAX) return -1;
+        return ans;
     }
 };
