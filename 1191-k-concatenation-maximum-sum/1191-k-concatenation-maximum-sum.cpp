f8b53fda2f250c5701463324d12482b343a3846d
Anirudh Agnihotri <72650662+AnirudhAgnihotri2902@users.noreply.github.com>
2022-07-21 02:28:06 +0530

    Time: 137 ms (10.34%), Space: 39.6 MB (44.38%) - LeetHub

---
+++ 1191-k-concatenation-maximum-sum/1191-k-concatenation-maximum-sum.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int kConcatenationMaxSum(vector<int>& arr, int k) {
+        int mod = 1000000007;
+        long long int zero = 0;
+        long long int res = arr[0];
+        long long int maxi = arr[0];
+        long long int n = arr.size();
+        for(int i = 1; i<n; i++){
+            maxi = max((long long int)arr[i], arr[i]+maxi);
+            res = max(res, maxi);
+            res%=mod;
+        }
+        res = max(res, zero);
+        long long int presum = 0;
+        long long int sufsum = 0;
+        long long int sum = 0;
+        maxi = 0;
+        for(int i = 0; i<n; i++){
+            presum+=arr[i];
+            maxi = max(maxi, presum);
+            sum+=arr[i];
+        }
+        presum = maxi;
+        sum%=mod;
+        sum = max(sum, zero);
+        presum%=mod;
+        maxi = 0;
+        for(int i = n-1; i>=0; i--){
+            sufsum+=arr[i];
+            maxi = max(maxi, sufsum);
+        }
+        maxi = max(sufsum, maxi);
+        sufsum = maxi;
+        sufsum%=mod;
+        long long int ans;
+        cout<<presum<<" "<<sufsum<<" "<<sum<<endl;
+        if(k == 1){
+            return res;
+        }
+        ans = max(res,(presum+ sufsum+ (sum*(k-2))%mod))%mod;
+        return (int)ans;
+    }
+};
