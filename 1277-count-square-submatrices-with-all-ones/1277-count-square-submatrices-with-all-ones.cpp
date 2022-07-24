cb834bb1a9b5477fb7a0f6619f68106f5549c60f
Anirudh Agnihotri <72650662+AnirudhAgnihotri2902@users.noreply.github.com>
2022-07-24 23:02:57 +0530

    Time: 147 ms (17.41%), Space: 23.6 MB (91.70%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
@@ -4,24 +4,22 @@ public:
         int sum = 0;
         int n = matrix.size();
         int m = matrix[0].size();
-        vector<vector<int>>dp(n, vector<int>(m,0));
+        //vector<vector<int>>dp(n, vector<int>(m,0));
         for(int i = 0; i<n; i++){
-            dp[i][0] = matrix[i][0];
-            sum+=dp[i][0];
+            sum+=matrix[i][0];
         }
         for(int j = 1; j<m; j++){
-            dp[0][j] = matrix[0][j];
-            sum+=dp[0][j];
+            sum+=matrix[0][j];
         }
         for(int i = 1; i<n; i++){
             for(int j = 1; j<m; j++){
                 if(matrix[i][j] == 0){
-                    dp[i][j] = 0;
+                    continue;
                 }
                 else{
-                    dp[i][j] = 1+ min(dp[i][j-1],min( dp[i-1][j], dp[i-1][j-1]));
+                    matrix[i][j] = 1+ min(matrix[i][j-1],min( matrix[i-1][j], matrix[i-1][j-1]));
                 }
-                sum+=dp[i][j];
+                sum+= matrix[i][j];
             }
         }
         return sum;
