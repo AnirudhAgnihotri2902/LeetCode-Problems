b57f2a6a9c6847c1ff3f18763c146f2f9e9a684e
Anirudh Agnihotri <72650662+AnirudhAgnihotri2902@users.noreply.github.com>
2022-09-09 23:35:57 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (17.85%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     unordered_map<char, vector<char>>map;
     vector<string>ans;
-    
     void solve(string &digits, int i, int n, string s){
         if(i == n){
             if(s == ""){
@@ -15,7 +14,6 @@ public:
             solve(digits, i+1, n,s+itr);
         }
     }
-    
     vector<string> letterCombinations(string digits) {
         map['2'] = {'a','b','c'};
         map['3'] = {'d','e','f'};
@@ -28,7 +26,6 @@ public:
         int n = digits.size();
         string s = "";
         solve(digits, 0, n ,s);
-        
         return ans;
     }
 };
